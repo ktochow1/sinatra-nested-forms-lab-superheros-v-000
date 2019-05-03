6a0a0a976e4e31fc7b84691f57980603d3ac556e
Kat Tochowicz <ktochow1@binghamton.edu>
2019-05-03 14:50:54 +0000

    Automatically backed up by Learn

--- app/models/superheroes.rb
+++ app/models/superheroes.rb
@@ -1,16 +1,18 @@
-class Superheroes
+class SuperHero
   attr_accessor :name, :power, :bio
-  
-  ALL_SUPERHEROES = []
-  
-  def initialize(args)
-    @name = args[:name]
-    @power = args[:power]
-    @bio = args[:bio]
-    ALL_SUPERHEROES << self
-  end 
-  
-  def self.all 
-    ALL_SUPERHEROES
+
+  def self.all
+    @@all ||= []
+  end
+
+  def initialize(opts)
+    @name  = opts[:name]
+    @power = opts[:power]
+    @bio   = opts[:bio]
+    self.save
+  end
+
+  def save
+    self.class.all << self
   end
 end
